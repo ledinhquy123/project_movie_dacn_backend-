76dd54e70a8eccc429f98eea864e0b8d7c95610c
ledinhquy123 <ledinhquy856@gmail.com>
2024-03-23 19:50:03 +0700

    hoan thanh backend

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Group;
+use App\Models\Screen;
+use App\Models\Seat;
+use App\Models\Transaction;
+use App\Models\Weekday;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Groups
+        Group::create([
+            'name' => 'user'
+        ]);
+        Group::create([
+            'name' => 'admin'
+        ]);
+
+        // Weekdays
+        for($i = 2; $i <= 7; $i++){
+            Weekday::create([
+                'name' => 'Thứ '.$i
+            ]);
+        }
+        Weekday::create([
+            'name' => 'CN'
+        ]);
+
+        // Screens and seats
+        for($i = 1; $i <= 10; $i++){
+            Screen::create([
+                'name' => 'Phòng '.$i
+            ]);
+
+            for($j = 1; $j <= 50; $j++){
+                Seat::create([
+                    'name' => chr(64 + $i) . $j,
+                    'screen_id' => $i,
+                    'status' => json_encode([])
+                ]);
+            }
+        }
+
+        // Transactions_type
+        Transaction::create([
+            'name' => 'VN Pay'
+        ]);
+        
+        Transaction::create([
+            'name' => 'Momo'
+        ]);
+        Transaction::create([
+            'name' => 'Zalo Pay'
+        ]);
+    }
+}
